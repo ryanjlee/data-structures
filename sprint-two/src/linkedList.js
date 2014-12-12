dc5270a5e157302cade50241e2bd87beadcd826c
ryanjlee <ryanjlee12@gmail.com>
2014-12-11 22:05:02 -0800

    Complete up to removehead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    newNode.next = list.tail;
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next =  newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target){
+    var check = function(obj) {
+      if (obj.value == target) {
+        return true;
+      }
+      if (obj.next === list.tail) {
+      check(obj.next);
+      }
+      return false;
+    };
+    check(list.head);
   };
 
   return list;
