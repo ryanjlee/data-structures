36217d647c9dda7eefc995226737b7dc0f524118
ryanjlee <ryanjlee12@gmail.com>
2014-12-12 20:45:23 -0800

    Complete to half of hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,23 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // check for info, if not there, set
+  if (!this._storage.get(i)) {
+    this._storage.set(i, [k, v]);
+  }
+  var tuple = this._storage.get(i);
+  tuple.push(k, v);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i)[1];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, [null, null]);
 };
 
 
