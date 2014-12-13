58dd0852717c8b426eb6e71f1945c436d8c4f213
ryanjlee <ryanjlee12@gmail.com>
2014-12-12 16:24:55 -0800

    Complete through Set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  for (var key in this.storage) {
+    if (this.storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this.storage[item];
 };
 
 /*
